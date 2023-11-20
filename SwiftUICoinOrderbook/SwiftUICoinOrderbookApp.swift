185a92b6d8a8d0e36752af876bd650c4c006b680
sumanhong <suman0204@naver.com>
2023-11-20 11:46:16 +0900

    Initial Commit

---
+++ SwiftUICoinOrderbook/SwiftUICoinOrderbookApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICoinOrderbookApp.swift
+//  SwiftUICoinOrderbook
+//
+//  Created by 홍수만 on 2023/11/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICoinOrderbookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
